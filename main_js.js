2638b18c072d91900b4b0b68fc3d6de49f7854fa
Nikfavorit89 <nikfavorit@mail.ru>
2019-07-16 17:38:12 +0300

    1

--- main_js.js
+++ main_js.js
@@ -1,2 +1,11 @@
 'use strict'
-a = 2
+
+let Cels = parseInt(prompt('Введите температуру в градусах по Цельсию: '));
+let Far = (9/5)*Cels+32;
+alert('Температурв в градусах по Фарингейту: ' + Far);
+
+
+let admin , name = null;
+name = "Василий";
+admin = name;
+console.log('Администратор -  ' + admin);
